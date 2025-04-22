14e8a0e68f59748f2242b0837eaacd88ff9f3e39
Matias <matias.costamagna@mi.unc.edu.ar>
2025-04-22 09:52:17 -0300

    Eliminacion del map

--- src/Pedido.java
+++ src/Pedido.java
@@ -4,14 +4,6 @@ public class Pedido {
     private final String id;
     private final int idCasillero;
     private EstadoPedido estado;
-    private static final Map<EstadoPedido, Set<EstadoPedido>> transicionesEstado = new HashMap<>();
-
-    static {
-        transicionesEstado.put(EstadoPedido.EN_PREPARACION, Set.of(EstadoPedido.EN_TRANSITO, EstadoPedido.FALLIDO));
-        transicionesEstado.put(EstadoPedido.EN_TRANSITO, Set.of(EstadoPedido.ENTREGADO, EstadoPedido.FALLIDO));
-        transicionesEstado.put(EstadoPedido.ENTREGADO, Set.of(EstadoPedido.VERIFICADO, EstadoPedido.FALLIDO));
-
-    }
 
     public Pedido(String id, int idCasillero) {
         this.id = id;
@@ -27,12 +19,20 @@ public class Pedido {
         return idCasillero;
     }
 
-    // Control de estado
-    public synchronized void setEstado(EstadoPedido nuevoEstado) {
-        Set<EstadoPedido> posibles = transicionesEstado.getOrDefault(this.estado, Set.of());
-
-        if (posibles.contains(nuevoEstado)) {
-            this.estado = nuevoEstado;
+    public synchronized void setEstado(EstadoPedido estado) {
+        // Validar que la transición de estado sea permitida
+        if (this.estado == EstadoPedido.EN_PREPARACION && estado == EstadoPedido.EN_TRANSITO) {
+            this.estado = estado;
+        } else if (this.estado == EstadoPedido.EN_PREPARACION && estado == EstadoPedido.FALLIDO) {
+            this.estado = estado;
+        } else if (this.estado == EstadoPedido.EN_TRANSITO && estado == EstadoPedido.ENTREGADO) {
+            this.estado = estado;
+        } else if (this.estado == EstadoPedido.EN_TRANSITO && estado == EstadoPedido.FALLIDO) {
+            this.estado = estado;
+        } else if (this.estado == EstadoPedido.ENTREGADO && estado == EstadoPedido.FALLIDO) {
+            this.estado = estado;
+        } else if (this.estado == EstadoPedido.ENTREGADO && estado == EstadoPedido.VERIFICADO) {
+            this.estado = estado;
         } else {
             System.out.println("[ERROR] Transición de estado no válida para el pedido #" + this.id);
         }
